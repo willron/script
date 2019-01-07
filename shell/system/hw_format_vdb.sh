13717984d4f85036155bab4384d1d799cd247a0f
will <willron@qq.com>
2019-01-07 12:34:59 +0800

    Create hw_format_vdb.sh

---
+++ shell/system/hw_format_vdb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#华为云服务器格式化vdb数据盘并挂载/data
+#也可用于其他云服务器
+#/data下会创建logs和www目录
+
+echo 
+echo "This Script Will Format /dev/vdb And Mount To /data, Are You Sure ..."
+read -p "(Please input 'Y/N' ! Default N) : " OPTION
+echo
+
+if [[ "$OPTION" == "" || "$OPTION" == "N" ]];then
+    echo "Bye"
+    exit 2
+fi
+
+if [ "$OPTION" == "Y" ];then
+    parted /dev/vdb --script -- mklabel msdos
+    parted /dev/vdb --script -- mkpart primary 0 -1
+    mkfs.ext4 /dev/vdb1
+    mount /dev/vdb1 /data
+    mkdir -p /data/logs/{php,nginx}
+    mkdir /data/www
+    chown www.www -R /data/{logs,www}
+    echo '/dev/vdb1  /data ext4    defaults    0  0' >> /etc/fstab
+    sed -i '3,$s/^/#/' $0
+fi
